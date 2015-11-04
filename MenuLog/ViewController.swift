2ca12722da401ce3e6038cc8b6ec76d5c19a659e
ezura yuka <tf_member@ezura.local>
2015-11-04 15:19:48 +0900

    init

---
+++ MenuLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuLog
+//
+//  Created by yuka ezura on 2015/11/04.
+//  Copyright © 2015年 ezura yuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
