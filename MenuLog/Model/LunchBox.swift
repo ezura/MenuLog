75340c552b2bfea2720b7d2799a27b5367682a3f
ezura yuka <tf_member@ezura.local>
2015-11-10 18:40:31 +0900

    [realm] replace 'Realm' to 'RealmSwift'

---
+++ MenuLog/Model/LunchBox.swift
@@ -0,0 +1,19 @@
+//
+//  LunchBox.swift
+//  MenuLog
+//
+//  Created by yuka ezura on 2015/11/10.
+//  Copyright © 2015年 ezura yuka. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class LunchBox: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
