3b29f10a46dd571089f8baa087b1468ea68ec43d
ezura yuka <tf_member@ezura.local>
2015-11-10 20:06:13 +0900

    [WIP] writing lunch box model

--- MenuLog/Model/LunchBox.swift
+++ MenuLog/Model/LunchBox.swift
@@ -11,6 +11,18 @@ import RealmSwift
 
 class LunchBox: Object {
     
+    dynamic private var imageURI = ""
+    
+    private var image: UIImage? {
+        get {
+            return _image
+        }
+        set(image) {
+            _image = image
+        }
+    }
+    lazy var _image: UIImage? = UIImage(contentsOfFile: self.imageURI)
+    
 // Specify properties to ignore (Realm won't persist these)
     
 //  override static func ignoredProperties() -> [String] {
