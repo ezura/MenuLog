2cec8610e2f9aa56b4caa5417177a23921e2412d
ezura yuka <tf_member@ezura.local>
2015-11-10 12:13:55 +0900

    [realm] set up

---
+++ Pods/Target Support Files/Pods-MenuLogTests/Pods-MenuLogTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MenuLogTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MenuLogTestsVersionString[];
+
---
+++ Support/library-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  library-Bridging-Header.h
+//  MenuLog
+//
+//  Created by yuka ezura on 2015/11/10.
+//  Copyright © 2015年 ezura yuka. All rights reserved.
+//
+
+#ifndef library_Bridging_Header_h
+#define library_Bridging_Header_h
+
+#import <Realm/Realm.h>
+
+#endif /* library_Bridging_Header_h */
